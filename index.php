df18cc54f935eda2f2cfa58f93c53280d25fa6dd
Marie <mari400m@edu.zealand.dk>
2022-11-08 11:42:34 +0100

    opstart

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html lang="da">
+<head>
+
+    <meta charset="utf-8">
+
+    <title>Sigende titel</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+    Tom side
+    <?php
+    $tekst = "Dette er en tekst";
+    $tal1 = 125;
+    $tal2 = 250;
+    $talResultat = $tal1 + $tal2;
+    echo "<br>Tal 1 er: " . $talResultat;
+    ?>
+
+</body>
+</html>
