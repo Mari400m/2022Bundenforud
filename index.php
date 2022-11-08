01d96f186175c71c3cf1d258f49a719fde8f4e9d
Marie <mari400m@edu.zealand.dk>
2022-11-08 14:02:48 +0100

    opstart

--- index.php
+++ index.php
@@ -1,29 +1,8 @@
-<!DOCTYPE html>
+<?php
+require "settings/init.php";
 
-<html lang="da">
-<head>
+$blogs = $db->sql("SELECT * FROM blogs");
 
-    <meta charset="utf-8">
-
-    <title>Sigende titel</title>
-
-    <meta name="robots" content="All">
-    <meta name="author" content="Udgiver">
-    <meta name="copyright" content="Information om copyright">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-
-<body>
-
-    Tom side
-    <?php
-    $tekst = "Dette er en tekst";
-    $tal1 = 125;
-    $tal2 = 250;
-    $talResultat = $tal1 + $tal2;
-    echo "<br>Tal 1 er: " . $talResultat;
-    ?>
-
-</body>
-</html>
+foreach ($blogs as $blog){
+    echo $blog->blogName . "<br>";
+}
